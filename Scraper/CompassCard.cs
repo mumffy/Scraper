4139163d246dbfeb6343ae2d3a10c5fd0f111cd0
mumffy <mumffy@gmail.com>
2017-07-29 13:00:47 -0700

    extracting cardNumber and password into method parameters

--- Scraper/CompassCard.cs
+++ Scraper/CompassCard.cs
@@ -12,8 +12,8 @@ namespace Scraper
         private const string url_login = @"https://compasscard.511sd.com/webtix/welcome/welcome.do";
         private static HttpClient hc = new HttpClient();
 
-        private readonly string password = ConfigurationManager.AppSettings["secretPassword"];
-        private readonly List<string> cardNumbers = new List<string>(ConfigurationManager.AppSettings["cardNumberCsv"].Split(','));
+        //private readonly string password = ConfigurationManager.AppSettings["secretPassword"];
+        //private readonly List<string> cardNumbers = new List<string>(ConfigurationManager.AppSettings["cardNumberCsv"].Split(','));
 
         public CompassCard()
         {
@@ -26,10 +26,10 @@ namespace Scraper
             return response.Content.ReadAsStringAsync().Result;
         }
 
-        public float LoginToSummaryPage()
+        public float GetRemainingBalance(string cardNumber, string password)
         {
             var listOfKeyValuePairs = new List<KeyValuePair<string, string>> {
-                new KeyValuePair<string, string>("cardNum", cardNumbers[0]),
+                new KeyValuePair<string, string>("cardNum", cardNumber),
                 new KeyValuePair<string, string>("pass", password),
                 new KeyValuePair<string, string>("cardOps","Display"),
             };
